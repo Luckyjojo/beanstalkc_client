a319ee534883b0073a74e46e80afbbad0b6a5001
lishixuan <jojo1776568@163.com>
2016-11-03 15:58:23 +0800

    Update simple_client.py

--- simple_client.py
+++ simple_client.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 #-*-coding:utf8-*-
-# authored by Jojo
-# email lishixuanmail@gmail.com
+
 import beanstalkc
 import json
+from ConfigParser import ConfigParser
 
 class beanstalkClient(object):
     """docstring for message"""
-    def __init__(self):
-        self.localhost = "192.168.50.42"
-        self.port = 11300
-        self.bar = "feed_result"
+    def __init__(self,conf_path):
+        self.cf = ConfigParser()
+        self.cf.read(conf_path)
+        self.localhost = self.cf.get("beanstalk","localhost")
+        self.port = self.cf.get("beanstalk","port")
+        self.pull_count = self.cf.get("beanstalk","count")
+
 
     def connect(self):
         try:
@@ -20,10 +23,10 @@ class beanstalkClient(object):
             return False
         return True
 
-    def put(self,message):
+    def put(self,message,bar):
         try:
             if self.connect():
-                self.bstk.watch(self.bar)
+                self.bstk.watch(bar)
                 self.bstk.put(message)
                 return True
             return False
@@ -31,12 +34,12 @@ class beanstalkClient(object):
             print err
             return False
 
-    def pull(self,bury = True,delete=False,pull_limit = 3):
+    def pull(self,bar,bury = True,delete=False,pull_limit = self.pull_count):
         try:
             results = []
             if self.connect():
                 for i in range(pull_limit):
-                    self.bstk.watch(self.bar)
+                    self.bstk.watch(bar)
                     job = self.bstk.reserve()
                     result = job.body
                     result = json.loads(result)
